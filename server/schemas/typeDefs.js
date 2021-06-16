0eba147b35bd7c40ae8261024f653ebe434982f8
Megs <Megs.close@gmail.com>
2021-06-16 13:30:17 -0700

    teenage mutant ninja queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,5 @@
-// import the gql tagged template function
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
     type User {
         _id: ID
@@ -11,6 +9,7 @@ const typeDefs = gql`
         thoughts: [Thought]
         friends: [User]
     }
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -19,19 +18,34 @@ const typeDefs = gql`
         reactionCount: Int
         reactions: [Reaction]
     }
+
     type Reaction {
         _id: ID
         reactionBody: String
         createdAt: String
         username: String
     }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
-        thoughts(username: String): [Thought] 
+        thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -4,14 +4,17 @@ const { ApolloServer } = require("apollo-server-express");
 
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3012;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
